ed774bf85ff93232f076a4bafdad1c994de6f92f
luizgustavogg <luizgustavoggdev@gmail.com>
2025-04-16 19:21:27 -0300

    FEAT: CONDICIONAL(URL VALID, CARACTERES ESPECIAIS, ESPAÇO)

--- index.js
+++ index.js
@@ -9,6 +9,38 @@ App.use(express.json())
 App.post ('/shorten', async (req, res) => {
     let { urlOriginal, shortCode } = req.body
     urlOriginal.startsWith("https://") ? urlOriginal : urlOriginal = "https://"+urlOriginal
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    var verifySpace = /\s/g 
+    var urlValid = /^((www\.)?[a-z0-9\-]+(\.[a-z]{2,})+(\/.*)?|magnet:\?xt=urn:btih:[a-zA-Z0-9]+)$/i;
+
+
+    if(urlValid.test(urlOriginal)){ // Contem url original
+        return res.status(400).json({
+            "message": "Url precisa ser valida!",
+            "code": 400
+        })
+    }
+
+    if(format.test(shortCode)){
+        return res.status(400).json({ // Contem Caracteres Especiais
+            "message": "Contem caracteres especiais!",
+            "code": 400
+        }).end()
+    }
+
+    if(verifySpace.test(shortCode)){ // Contem Espaço no ShortCode! 
+        return res.status(400).json({
+            "message": "Não pode ter espaço no Shortcode!",
+            "code": 400
+        }).end()
+    }
+
+    if(shortCode.length > 30){ // Máximo de Caracteres
+        return res.status(400).json({
+            "message": "Máximo de caracteres atingida!",
+            "code": 400
+        }).end()
+    }
 
     const urlShort = await prisma.link.findUnique({
         where:{
@@ -19,13 +51,6 @@ App.post ('/shorten', async (req, res) => {
     if(urlShort){ // ShortCode ja existe
         return res.status(400).json({
             "message": "Este shortcode já esta sendo utilizado!",
-            "code": 400
-        }).end()
-    }
-
-    if(shortCode.length > 30){ // Máximo de Caracteres
-        return res.status(400).json({
-            "message": "Máximo de caracteres atingida!",
             "code": 400
         }).end()
     }
